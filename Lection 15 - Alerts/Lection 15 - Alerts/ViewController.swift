66f6d5e111a2043f22dec92b9f561c2c5cfabdf7
Viacheslav Bilyi <>
2019-07-08 21:35:28 +0300

    added alert

--- Lection 15 - Alerts/Lection 15 - Alerts/ViewController.swift
+++ Lection 15 - Alerts/Lection 15 - Alerts/ViewController.swift
@@ -15,6 +15,19 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view.
 	}
 
+	@IBAction func showAlert() {
+		let alert = UIAlertController(title: "Photo Access", message: "Do you alow to use your photos?", preferredStyle: .alert)
 
+		let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+		alert.addAction(okAction)
+
+		let cancelAcion = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+		alert.addAction(cancelAcion)
+
+		present(alert, animated: true, completion: nil)
+	}
+
+	@IBAction func showActionSheet() {
+	}
 }
 
