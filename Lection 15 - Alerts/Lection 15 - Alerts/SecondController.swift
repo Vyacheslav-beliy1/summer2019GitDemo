69106b366392768586f9cf2fb223d071abd8488c
Сергей Титов <sierj.titov.001@gmail.com>
2019-07-11 21:04:48 +0300

    created

---
+++ Lection 15 - Alerts/Lection 15 - Alerts/SecondController.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  Lection 15 - Alerts
+//
+//  Created by Sergey Titov on 7/11/19.
+//  Copyright © 2019 Viacheslav Bilyi. All rights reserved.
+//
+
+
+import UIKit
+
+class  SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+}
+
+
---
+++ Lection 15 - Alerts/Lection 15 - Alerts/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  Lection 15 - Alerts
+//
+//  Created by Sergey Titov on 7/11/19.
+//  Copyright © 2019 Viacheslav Bilyi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    @IBAction func NextButton(_ sender: Any) {
+        let nextButton = UIButton()
+        
+        
+    }
+    
+    
+    
+    
+    @IBAction func PreviousButton(_ sender: Any) {
+        let buttonPrevious = UIButton()
+        self.dismiss(animated: true, completion: nil)
+    }
+}
