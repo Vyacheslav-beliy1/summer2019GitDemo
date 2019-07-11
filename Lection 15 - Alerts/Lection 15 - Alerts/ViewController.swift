af07f56b9a2f34e6c09d9fbf035a33dc9d59cbbc
Viacheslav Bilyi <>
2019-07-11 19:25:12 +0300

    added new film;

--- Lection 15 - Alerts/Lection 15 - Alerts/ViewController.swift
+++ Lection 15 - Alerts/Lection 15 - Alerts/ViewController.swift
@@ -32,6 +32,7 @@ class ViewController: UIViewController {
 		enum Films: String {
 			case film1 = "Человек Паук"
 			case film2 = "Алладин"
+			case film3 = "Лысая гора"
 		}
 
 		let alert = UIAlertController(title: nil, message: "Choose Film", preferredStyle: .actionSheet)
@@ -54,6 +55,9 @@ class ViewController: UIViewController {
 
 		let film2 = UIAlertAction(title: Films.film2.rawValue, style: .default, handler: handler(.film2))
 		alert.addAction(film2)
+
+		let film3 = UIAlertAction(title: Films.film3.rawValue, style: .default, handler: handler(.film3))
+		alert.addAction(film3)
 
 		present(alert, animated: true, completion: nil)
 	}
