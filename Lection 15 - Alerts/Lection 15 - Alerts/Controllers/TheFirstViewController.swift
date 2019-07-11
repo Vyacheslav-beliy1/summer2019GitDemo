93b8a466af4b61cda3e3399cd9bfebdb5dcbc3bc
3ai41k <nikitalizogubov2000@gmail.com>
2019-07-11 20:45:38 +0300

    added the FirstViewController

---
+++ Lection 15 - Alerts/Lection 15 - Alerts/Controllers/TheFirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TheFirstViewController.swift
+//  Lection 15 - Alerts
+//
+//  Created by aud 613 on 7/11/19.
+//  Copyright © 2019 Viacheslav Bilyi. All rights reserved.
+//
+
+import UIKit
+
+class TheFirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
