1e307ca86a8a2f5ee365917c7183da4a47e56fca
Oleksii Perov <oleksii.perov@nure.ua>
2019-07-11 20:46:26 +0300

    added vc3 class

---
+++ Lection 15 - Alerts/Lection 15 - Alerts/ViewControllerThree.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerThree.swift
+//  Lection 15 - Alerts
+//
+//  Created by Алексей Перов on 7/11/19.
+//  Copyright © 2019 Viacheslav Bilyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerThree: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
