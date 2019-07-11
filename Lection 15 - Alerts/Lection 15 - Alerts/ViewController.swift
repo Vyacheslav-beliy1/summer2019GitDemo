febd750c8a41e928223d3371cbd2541143dd50ad
3ai41k <nikitalizogubov2000@gmail.com>
2019-07-11 19:23:21 +0300

    SkybiDu

--- Lection 15 - Alerts/Lection 15 - Alerts/ViewController.swift
+++ Lection 15 - Alerts/Lection 15 - Alerts/ViewController.swift
@@ -32,19 +32,20 @@ class ViewController: UIViewController {
 		enum Films: String {
 			case film1 = "Человек Паук"
 			case film2 = "Алладин"
+            case film3 = "Skubi Do"
 		}
 
 		let alert = UIAlertController(title: nil, message: "Choose Film", preferredStyle: .actionSheet)
 
-		let handler:(_ type: Films) -> (UIAlertAction) -> Void =
-		{ type in
-			//sdf/asdf//asd/f/asdf/
-			//asdfasdfasdf
-			return { action in
-				//asdf/asdf/as/df/asdf/a/sdf/asdf
-				print(type.rawValue)
-			}
-		}
+        let handler:(_ type: Films) -> (UIAlertAction) -> Void =
+        { type in
+            //sdf/asdf//asd/f/asdf/
+            //asdfasdfasdf
+            return { action in
+                //asdf/asdf/as/df/asdf/a/sdf/asdf
+                print(type.rawValue)
+            }
+        }
 
 		let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
 		alert.addAction(cancelAction)
@@ -54,6 +55,9 @@ class ViewController: UIViewController {
 
 		let film2 = UIAlertAction(title: Films.film2.rawValue, style: .default, handler: handler(.film2))
 		alert.addAction(film2)
+        
+        let film3 = UIAlertAction(title: Films.film3.rawValue, style: .default, handler: handler(.film3))
+        alert.addAction(film3)
 
 		present(alert, animated: true, completion: nil)
 	}
